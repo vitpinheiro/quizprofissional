e7d218c38413c49fe72ed9e8ae659ef980fe2224
vitpinheiro <vitoriapinheiro775@gmail.com>
2024-10-30 16:51:11 -0300

    Quiz Profissional base

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz Profissional</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="background-color: #12753C;">
+    <header></header>
+
+    <main>
+
+    <div class="centro">
+        <img class="gp" src="images/logohrg.png" alt="">
+        <p>lorem</p>
+        <img class="hrg" src="images/logogp.png" alt="">
+    </div>
+
+
+
+    </main>
+
+    
+</body>
+</html>
